07fa24bb5e418b8165b2a2a08a26e68c915fc012
APACHE-0002 <48190578+APACHE-0002@users.noreply.github.com>
2021-09-13 18:15:32 -0300

    fastcode

---
+++ fazt_code/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Profile } from './components/Profile';
+import { UserList } from './components/UserList';
+
+import './styles.css'
+
+export default function App() {
+  
+  return(
+    <>
+    <Profile/>
+    <UserList/>
+    </>
+  )
+}
